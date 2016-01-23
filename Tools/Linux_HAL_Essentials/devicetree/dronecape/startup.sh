58db8eca42cc813464169bd6c471dfdb1e409ad8
Steven Harsanyi <stevenharsanyi@gmail.com>
2016-01-23 02:40:45 -0500

    update droncape startup script

--- Tools/Linux_HAL_Essentials/devicetree/dronecape/startup.sh
+++ Tools/Linux_HAL_Essentials/devicetree/dronecape/startup.sh
@@ -1,8 +1,7 @@
 if [ "`echo $1`" = "load" ]; then
     echo "Loading Capes..."
 sudo sh -c "echo 'BB-DRONECAPE' >> /sys/devices/platform/bone_capemgr/slots"
-#    echo am33xx_pwm > /sys/devices/bone_capemgr.9/slots
-#    echo bone_pwm_P8_36 > /sys/devices/bone_capemgr.9/slots
+#sudo sh -c "echo 'am33xx_pwm' >> /sys/devices/platform/bone_capemgr/slots"
     dmesg | grep "SPI"
     dmesg | grep "PRU"
     cat /sys/devices/platform/bone_capemgr/slots
