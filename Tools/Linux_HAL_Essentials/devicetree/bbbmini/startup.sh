4f84c07a84db4051652932fbab44ee6b86a45dc0
Steven Harsanyi <stevenharsanyi@gmail.com>
2016-01-23 02:36:10 -0500

    updated bbbmini firmware cp updated scripts

--- Tools/Linux_HAL_Essentials/devicetree/bbbmini/startup.sh
+++ Tools/Linux_HAL_Essentials/devicetree/bbbmini/startup.sh
@@ -1,11 +1,11 @@
 if [ "`echo $1`" = "load" ]; then
     echo "Loading Capes..."
-    echo BB-BBBMINI > /sys/devices/bone_capemgr.9/slots
-    echo am33xx_pwm > /sys/devices/bone_capemgr.9/slots
-    echo bone_pwm_P8_36 > /sys/devices/bone_capemgr.9/slots
+    sudo sh -c "echo 'BB-BBBMINI' >> /sys/devices/platform/bone_capemgr/slots"
+    sudo sh -c "echo 'am33xx_pwm' >> /sys/devices/platform/bone_capemgr/slots"
+    sudo sh -c "echo 'bone_pwm_P8_36' >> /sys/devices/platform/bone_capemgr/slots"
     dmesg | grep "SPI"
     dmesg | grep "PRU"
-    cat /sys/devices/bone_capemgr.9/slots
+    cat /sys/devices/platform/bone_capemgr/slots
 else
     echo "Usage:"
     echo "      ./startup.sh load  : to load the capes"
--- Tools/Linux_HAL_Essentials/devicetree/dronecape/startup.sh
+++ Tools/Linux_HAL_Essentials/devicetree/dronecape/startup.sh
@@ -1,11 +1,11 @@
 if [ "`echo $1`" = "load" ]; then
     echo "Loading Capes..."
-    echo BB-DRONECAPE > /sys/devices/bone_capemgr.9/slots
-    echo am33xx_pwm > /sys/devices/bone_capemgr.9/slots
-    echo bone_pwm_P8_36 > /sys/devices/bone_capemgr.9/slots
+sudo sh -c "echo 'BB-DRONECAPE' >> /sys/devices/platform/bone_capemgr/slots"
+#    echo am33xx_pwm > /sys/devices/bone_capemgr.9/slots
+#    echo bone_pwm_P8_36 > /sys/devices/bone_capemgr.9/slots
     dmesg | grep "SPI"
     dmesg | grep "PRU"
-    cat /sys/devices/bone_capemgr.9/slots
+    cat /sys/devices/platform/bone_capemgr/slots
 else
     echo "Usage:"
     echo "      ./startup.sh load  : to load the capes"
