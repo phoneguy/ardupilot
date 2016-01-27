0c8b55a66a71306448480ccabf59186eacf08a8d
Steven Harsanyi <stevenharsanyi@gmail.com>
2016-01-27 00:04:05 -0500

    adding beaglebone black dronecape installer

--- bbb_upload.sh
+++ bbb_upload.sh
@@ -4,6 +4,7 @@
 # make bbb and copy to deploy directory
 # copy over to beaglebone to /home/debian/bin
 
+# update git
 #rm -rf deploy
 mkdir deploy
 git checkout bbb-mpu9250
@@ -11,13 +12,18 @@ git pull diydrones master
 git submodule update --init
 make clean
 make bbb -j4
+
+# copy to deploy dir
 cp ArduCopter/ArduCopter.elf deploy
 cp ArduPlane/ArduPlane.elf deploy
 cp APMrover2/APMrover2.elf deploy
 cp AntennaTracker/AntennaTracker.elf deploy
 cp Tools/Linux_HAL_Essentials/devicetree/dronecape/switch-vehicle.sh deploy
+cp Tools/Linux_HAL_Essentials/devicetree/dronecape/am335x-boneblack-dronecape.dtb deploy
 cp Tools/Linux_HAL_Essentials/devicetree/dronecape/BB-DRONECAPE-00A0.dtbo deploy
 cp Tools/Linux_HAL_Essentials/devicetree/dronecape/README.md deploy
 cp Tools/Linux_HAL_Essentials/devicetree/dronecape/startup.sh deploy
 
+# copy over to beaglebone
+# default password is    temppwd
 scp ~/ardupilot/deploy/*.* debian@192.168.2.30:~/bin
