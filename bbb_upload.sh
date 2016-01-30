fd6a621b1d971dd338f3462e8f8827b2a265b618
Steven Harsanyi <stevenharsanyi@gmail.com>
2016-01-30 00:59:07 -0500

    add beaglebone black elf uploader add beaglebone to missionplanner log uploader

--- bbb_upload.sh
+++ bbb_upload.sh
@@ -4,6 +4,11 @@
 # make bbb and copy to deploy directory
 # copy over to beaglebone to /home/debian/bin
 
+LOCAL_ARDUPILOT_DIR=~/ardupilot
+UPLOAD_TARGET=192.168.2.30
+UPLOAD_USER=debian
+UPLOAD_DIR=~/bin
+
 # update git
 #rm -rf deploy
 mkdir deploy
@@ -18,13 +23,14 @@ cp ArduCopter/ArduCopter.elf deploy
 cp ArduPlane/ArduPlane.elf deploy
 cp APMrover2/APMrover2.elf deploy
 cp AntennaTracker/AntennaTracker.elf deploy
-cp Tools/Linux_HAL_Essentials/devicetree/dronecape/switch-vehicle.sh deploy
 cp Tools/Linux_HAL_Essentials/devicetree/dronecape/am335x-boneblack-dronecape.dtb deploy
 cp Tools/Linux_HAL_Essentials/devicetree/dronecape/BB-DRONECAPE-00A0.dtbo deploy
 cp Tools/Linux_HAL_Essentials/devicetree/dronecape/README.md deploy
 cp Tools/Linux_HAL_Essentials/devicetree/dronecape/startup.sh deploy
 cp Tools/Linux_HAL_Essentials/devicetree/dronecape/install.sh deploy
+cp Tools/Linux_HAL_Essentials/devicetree/dronecape/upload-logs.sh deploy
+cp Tools/Linux_HAL_Essentials/devicetree/dronecape/switch-vehicle.sh deploy
 
 # copy over to beaglebone
 # default password is    temppwd
-scp ~/ardupilot/deploy/*.* debian@192.168.2.145:~/bin
+scp ~/ardupilot/deploy/*.* debian@192.168.2.30:~/bin
