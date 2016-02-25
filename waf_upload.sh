c360310b21f272da75dc2a68a80eb34420b7037a
Steven Harsanyi <stevenharsanyi@gmail.com>
2016-02-25 18:02:12 -0500

    update waf build script for bbb

--- waf_upload.sh
+++ waf_upload.sh
@@ -35,8 +35,8 @@ echo "Update submodules and init new ones: "
 #git submodule update --init
 
 # clean, configure and build project
-#$WAF distclean
-#$WAF configure --board=$BUILD_TARGET
+$WAF distclean
+$WAF configure --board=$BUILD_TARGET
 #$WAF all -j4
 $WAF bin -j4
 
