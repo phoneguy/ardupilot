2d4cb957d09796b1ac6ca1fe2c922ac9c7981fc2
Steven Harsanyi <stevenharsanyi@gmail.com>
2016-03-12 09:31:34 -0500

    test different flow settings

--- libraries/AP_HAL/AP_HAL_Boards.h
+++ libraries/AP_HAL/AP_HAL_Boards.h
@@ -393,8 +393,8 @@
 /* Suyin CH0316 .3 MP board camera 640x480 320x240 160x120
  * focal length 2.2 mm, pixel size 5.6 um
  * 240x240 crop rescaled to 64x64 */
+#define HAL_FLOW_PX4_FOCAL_LENGTH_MILLIPX (3.5 / (2.1 * 2.0 * 240 / 64))
 //#define HAL_FLOW_PX4_FOCAL_LENGTH_MILLIPX (5.6 / (2.2 * 2.0 * 240 / 64))
-#define HAL_FLOW_PX4_FOCAL_LENGTH_MILLIPX (6.0 / (2.4 * 2.0 * 240 / 64))
 /* 1.3 MP board camera 1280x1024 640x480 320x240
  * focal length 2.4, pixel size 1.75 um
  * 240x240 crop rescaled to 64x64 */
@@ -403,7 +403,7 @@
  * focal length 2.1 mm, pixel size 3 um
  * 240x240 crop rescaled to 64x64 */
 //#define HAL_FLOW_PX4_FOCAL_LENGTH_MILLIPX (3.0 / (2.1 * 2.0 * 240 / 64))
-#define HAL_PARAM_DEFAULTS_PATH "/etc/ardpilot/arducopter.parm"
+//#define HAL_PARAM_DEFAULTS_PATH "/etc/ardpilot/arducopter.parm"
 
 #elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BH
 #define HAL_BOARD_LOG_DIRECTORY "/var/APM/logs"
