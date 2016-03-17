0b70a5e2fecd89fca50d03a392d0cce552d29ed0
Steven Harsanyi <stevenharsanyi@gmail.com>
2016-03-17 09:27:43 -0400

    update script

--- waf_upload.sh
+++ waf_upload.sh
@@ -37,8 +37,8 @@ git submodule update --recursive --init
 # clean, configure and build project
 $WAF distclean
 $WAF configure --board=$BUILD_TARGET
-#$WAF all -j4
-$WAF bin -j4
+$WAF all -j4
+#$WAF bin -j4
 
 # copy to deploy dir
 echo "Copying files to to deploy dir: "
